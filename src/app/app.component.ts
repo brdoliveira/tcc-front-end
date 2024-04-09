80ddd01536640eb18c422b9cc6adf270b8dc9176
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2024-04-09 20:46:12 -0300

    Criando estrutura inicial do projeto

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet,ButtonModule],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.sass']
+})
+export class AppComponent {}
