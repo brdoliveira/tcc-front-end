030b2a22f65b7cc17d81a26b6f4e5c058ef4dd12
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2024-04-11 20:52:23 -0300

    FEAT(sidebar): Criando componente sidebar

---
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrl: './sidebar.component.sass'
+})
+export class SidebarComponent implements OnInit {
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+}
