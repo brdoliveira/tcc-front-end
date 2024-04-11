b708136f36dad860ea10edea7e4c1044a8209498
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2024-04-11 20:57:51 -0300

    FEAT(home): Criando componente home

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'home',
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.sass'
+})
+export class HomeComponent {
+
+}
