b186a264428e2db563788d15092f57ed272b4155
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2024-04-12 23:59:03 -0300

    FEAT(app.module): Importando modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { SharedModule } from './shared/components/shared.module';
+import { AutoCompleteModule } from 'primeng/autocomplete';
 
 import { AppComponent } from './app.component';
-import { SharedModule } from './shared/components/shared.module';
-import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { HelpsComponent } from './pages/helps/helps.component';
+import { LoginComponent } from './pages/login/login.component';
+import { ContactFormComponent } from './pages/contact/contact-form/contact-form.component';
+import { DetailsComponent } from './pages/details/details.component';
 
 @NgModule({
-  declarations: [AppComponent,HomeComponent],
-  imports: [
-    SharedModule,
-    BrowserModule,
-    AppRoutingModule,
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    ContactFormComponent,
+    HelpsComponent,
+    LoginComponent,
+    DetailsComponent
   ],
+  imports: [SharedModule, BrowserModule, AppRoutingModule, AutoCompleteModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
