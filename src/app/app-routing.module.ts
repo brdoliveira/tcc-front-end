9254722e2f078ded879f7c0f5b78a9f312724692
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2024-04-12 23:58:18 -0300

    FEAT(app-routing): Adicionando novas rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes, CanActivate } from '@angular/router';
 import { NgModule } from '@angular/core';
 
 import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { HelpsComponent } from './pages/helps/helps.component';
+import { LoginComponent } from './pages/login/login.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { DetailsComponent } from './pages/details/details.component';
+import { AuthGuard } from './core/authentication/auth.guard';
 
 const routes: Routes = [
-    { path: 'home', component: HomeComponent },
+    { path: '/home', component: HomeComponent },
+    { path: '/helps', component: HelpsComponent},
+    { path: '/contact', component: ContactComponent},
+    { path: '/about', component: AboutComponent},
+    { path: '/login', component: LoginComponent},
+    { path: '/details', component: DetailsComponent, canActivate: [AuthGuard]},
     { path: '', redirectTo: '/home', pathMatch: 'full' },
 ];
 
