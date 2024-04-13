2977e89395602eeb79235ddabcdcc6a8724156a6
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2024-04-13 00:03:10 -0300

    FEAT(box): Criando componente box

---
+++ src/app/shared/components/box/box.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-box',
+  templateUrl: './box.component.html',
+  styleUrl: './box.component.scss'
+})
+export class BoxComponent {
+  @Input() icon: string = '';
+  @Input() title: string = '';
+  @Input() text: string = '';
+}
