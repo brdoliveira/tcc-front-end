0b13ddfb75293cbaf9384457a3a378a29b9588d8
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2024-04-13 00:01:57 -0300

    FEAT(helps): Criando componente de ajuda

---
+++ src/app/pages/helps/helps.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'helps',
+  templateUrl: './helps.component.html',
+  styleUrl: './helps.component.scss'
+})
+export class HelpsComponent {
+
+}
