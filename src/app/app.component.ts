444399a3755f288989f6ac445dc053e287589b74
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2024-04-12 23:58:45 -0300

    FEAT(app): Adicionando menus

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from './core/authentication/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = "000"
+
+  constructor(public authService: AuthService){}
 }
